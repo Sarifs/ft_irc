99fec022671469c67363389e09d20de963dd9043
Zedsfr <167260856+Zedsfr@users.noreply.github.com>
2025-08-23 16:42:26 +0200

    Add files via upload

---
+++ Client.hpp
@@ -0,0 +1,29 @@
+
+
+#ifndef CLIENT_HPP
+#define CLIENT_HPP
+
+#include <iostream>
+#include <string>
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <atomic>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#include "Parsing.hpp"
+
+struct ClientState{
+    std::mutex      sendMutex;
+    std::queue<std::string>  sendQueue;
+    std::atomic<bool>   running{true};
+};
+
+void    sendThread(int sock, ClientState& state);
+int     conenctToServer(const std::string& host, int port);
+void    receiveLoop(int sock, ClientState& state);
+void    userInputLoop(ClientState&  state);
+
+#endif
