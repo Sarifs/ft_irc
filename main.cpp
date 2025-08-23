cf95e5d4b832d31228a0bdbbbcfacad719b81667
Zedsfr <167260856+Zedsfr@users.noreply.github.com>
2025-08-23 16:42:54 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -17,45 +17,45 @@ extern  IRCMessage  parseIRCMessage(const std::string& raw);
 //     return 0;
 // }
 
-int main() {
-    // Simule une réponse PING
-    handleIRCMessage({"", "PING", {"123456"}});
+// int main() {
+//     // Simule une réponse PING
+//     handleIRCMessage({"", "PING", {"123456"}});
 
-    // Simule un message PONG
-    handleIRCMessage({"", "PONG", {}});
+//     // Simule un message PONG
+//     handleIRCMessage({"", "PONG", {}});
 
-    // Simule un message PRIVMSG
-    handleIRCMessage({"nick!user@host", "PRIVMSG", {"#channel", "Hello everyone!"}});
+//     // Simule un message PRIVMSG
+//     handleIRCMessage({"nick!user@host", "PRIVMSG", {"#channel", "Hello everyone!"}});
 
-    // Simule un message NOTICE
-    handleIRCMessage({"server", "NOTICE", {"#channel", "This is a notice."}});
+//     // Simule un message NOTICE
+//     handleIRCMessage({"server", "NOTICE", {"#channel", "This is a notice."}});
 
-    // Simule un JOIN
-    handleIRCMessage({"nick!user@host", "JOIN", {"#channel"}});
+//     // Simule un JOIN
+//     handleIRCMessage({"nick!user@host", "JOIN", {"#channel"}});
 
-    // Simule un PART
-    handleIRCMessage({"nick!user@host", "PART", {"#channel"}});
+//     // Simule un PART
+//     handleIRCMessage({"nick!user@host", "PART", {"#channel"}});
 
-    // Simule un QUIT
-    handleIRCMessage({"nick!user@host", "QUIT", {"Goodbye!"}});
+//     // Simule un QUIT
+//     handleIRCMessage({"nick!user@host", "QUIT", {"Goodbye!"}});
 
-    // Simule un changement de nick
-    handleIRCMessage({"oldnick!user@host", "NICK", {"newnick"}});
+//     // Simule un changement de nick
+//     handleIRCMessage({"oldnick!user@host", "NICK", {"newnick"}});
 
-    // Simule un changement de topic
-    handleIRCMessage({"nick!user@host", "TOPIC", {"#channel", "New topic of the channel"}});
+//     // Simule un changement de topic
+//     handleIRCMessage({"nick!user@host", "TOPIC", {"#channel", "New topic of the channel"}});
 
-    // Simule un changement de mode
-    handleIRCMessage({"nick!user@host", "MODE", {"#channel", "+o", "nick"}});
+//     // Simule un changement de mode
+//     handleIRCMessage({"nick!user@host", "MODE", {"#channel", "+o", "nick"}});
 
-    // Simule un message 001 (welcome)
-    handleIRCMessage({"", "001", {"nick", "Welcome to IRC!"}});
+//     // Simule un message 001 (welcome)
+//     handleIRCMessage({"", "001", {"nick", "Welcome to IRC!"}});
 
-    // Simule une erreur de nick déjà pris
-    handleIRCMessage({"", "433", {"*", "nick", "Nickname is already in use."}});
+//     // Simule une erreur de nick déjà pris
+//     handleIRCMessage({"", "433", {"*", "nick", "Nickname is already in use."}});
 
-    // Simule une commande inconnue
-    handleIRCMessage({"", "XYZ", {"some", "random", "params"}});
+//     // Simule une commande inconnue
+//     handleIRCMessage({"", "XYZ", {"some", "random", "params"}});
 
-    return 0;
-}
+//     return 0;
+// }
